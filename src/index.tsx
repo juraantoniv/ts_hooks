1fdbf4a5dd59939fc6feac14cb761e6e72b88fac
juraantoniv <juraantoniv@gmail.com>
2023-02-17 22:55:38 +0200

    ok

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import {App} from './App';
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
+import {setupStore} from "./redax/store";
 
+
+const store = setupStore();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-    <Provider store={}>
+    <Provider store={store}>
     <BrowserRouter>
         <App />
     </BrowserRouter>
